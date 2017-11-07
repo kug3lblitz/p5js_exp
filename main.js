54329fcae102d60e155365069e76b3c95e2837c6
Patrick Werner <pwerner2@gmail.com>
2017-11-07 16:17:26 -0500

    animated

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+let maxCircleSize = 20
+let phase = 0, speed = 0.03;
+
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
   background(127);
-  var x = width/2, y = height/2;
-  ellipse(x, y, 50, 50);
+  let x = width/2;
+  let y = height/2 + sin(phase) * 50;
+  phase = frameCount * speed;
+  let sizeOffset = (cos(phase) + 1) * 0.5;
+  let circleSize = sizeOffset * maxCircleSize;
+
+  ellipse(x, y, circleSize, circleSize);
 }
